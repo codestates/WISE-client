42a8a99c9370a60db158f35c991b804d01e5a29e
Gahee Kim <rkgml981105@gmail.com>
2021-05-14 01:18:54 +0900

    add css transition &  more pages to landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,25 +3,22 @@ import { useEffect } from 'react';
 import axios from 'axios';
 import Layout from '../components/Layout';
 
-const IndexPage = (): JSX.Element => {
-    const handleTest = async () => {
-        const response = await axios.get('https://api-wise.com/');
-        console.log(response.data);
-    };
-    useEffect(() => {
-        handleTest();
-    }, []);
-    return (
-        <Layout>
-            <h1>Hello World 👋</h1>
-            <p>
-                <Link href="/landing">
-                    <a>About</a>
-                </Link>
-            </p>
-            <div />
-        </Layout>
-    );
-};
-
+const IndexPage = (): JSX.Element => (
+    // const handleTest = async () => {
+    //     const response = await axios.get('https://api-wise.com/');
+    //     console.log(response.data);
+    // };
+    // useEffect(() => {
+    //     handleTest();
+    // }, []);
+    <Layout>
+        <h1>Hello World 👋</h1>
+        <p>
+            <Link href="/landing">
+                <a>About</a>
+            </Link>
+        </p>
+        <div />
+    </Layout>
+);
 export default IndexPage;
