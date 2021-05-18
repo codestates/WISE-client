52cc8ae4809b435490e93c6a4ba6cb6512e6f272
tjdgus3160 <tjdgus3160@gmail.com>
2021-05-19 03:56:41 +0900

    api key hidden

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 module.exports = {
     env: {
         IMPcode: 'imp57278971',
+        apiKey: 'AIzaSyAMOvQAqwdBVWn942rEY5YID8z9NtvQwlc',
+        authDomain: 'wise-4bb4c.firebaseapp.com',
+        projectId: 'wise-4bb4c',
+        storageBucket: 'wise-4bb4c.appspot.com',
+        messagingSenderId: '1056980953893',
+        appId: '1:1056980953893:web:1f9f1399e37e3650a2c8cc',
     },
 };
