1824e06bd95042d9289ead78977d74923e6a5978
Gahee Kim <rkgml981105@gmail.com>
2021-05-19 19:53:45 +0900

    add reducer & actions for detail page

--- reducers/index.js
+++ reducers/index.js
@@ -1,11 +1,9 @@
 import { HYDRATE } from 'next-redux-wrapper';
 import { combineReducers } from 'redux';
 
-<<<<<<< HEAD
 import user from './user';
+import service from './service';
 
-=======
->>>>>>> cf58c611a460e5843f7198676fc32e50e7cb851a
 const rootReducer = combineReducers({
     index: (state = {}, action) => {
         switch (action.type) {
@@ -15,10 +13,8 @@ const rootReducer = combineReducers({
                 return state;
         }
     },
-<<<<<<< HEAD
     user,
-=======
->>>>>>> cf58c611a460e5843f7198676fc32e50e7cb851a
+    service,
 });
 
 export default rootReducer;
