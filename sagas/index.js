ccbc8e06774f7a6e9497cab0d1fdf57653e9cbc5
tjdgus3160 <tjdgus3160@gmail.com>
2021-05-21 00:09:32 +0900

    add redux saga

---
+++ sagas/index.js
@@ -0,0 +1,11 @@
+import { all, fork } from 'redux-saga/effects';
+import axios from 'axios';
+
+import userSaga from './user';
+
+axios.defaults.baseURL = process.env.backUrl;
+axios.defaults.withCredentials = true;
+
+export default function* rootSaga() {
+    yield all([fork(userSaga)]);
+}
