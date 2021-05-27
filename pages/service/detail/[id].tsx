5c4b1a178dbb9b0d19d458cbf3dacbfa16698c41
Gahee Kim <rkgml981105@gmail.com>
2021-05-27 15:54:25 +0900

    fix error for deployment test

--- pages/service/detail/[id].tsx
+++ pages/service/detail/[id].tsx
@@ -35,7 +35,8 @@ const ServiceDetail = () => {
     // const { id } = router.query;
     // console.log(id);
 
-    const { service, review } = useSelector((state: RootState) => state.service);
+    // TODO: review import
+    const { service } = useSelector((state: RootState) => state.service);
 
     // carousel
     const slider = useRef<HTMLInputElement>(null);
@@ -81,7 +82,8 @@ const ServiceDetail = () => {
                                     </CarouselCon>
                                     <Navigation _id={service._id} />
                                     <Description service={service} />
-                                    <ReviewComponent review={review} />
+                                    {/* <ReviewComponent review={review} /> */}
+                                    <ReviewComponent />
                                     <FAQ />
                                     <Refund />
                                 </Detail>
